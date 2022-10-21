d44e17cfff3ab5076f2fcf55867ba50d6c8b6979
Tim Crowe <tim@woodpigeon.com>
2022-10-21 14:47:12 +0100

    Remove local_patient_id from uniq index migration for now
    To avoid deployment issues at BLT

--- db/migrate/20210818142811_add_unique_indexes_to_local_patient_ids1.rb
+++ db/migrate/20210818142811_add_unique_indexes_to_local_patient_ids1.rb
@@ -1,8 +1,11 @@
 class AddUniqueIndexesToLocalPatientIds1 < ActiveRecord::Migration[5.2]
   def change
+    # Removing local_patient_id from uniq index migration as it causes issues at Barts where there
+    # are ~100 patients with a non-unique local_patient_id.
+    # local_patient_id can be added back into the array in this migration once it has been run in
+    # a BLT
     within_renalware_schema do
       %i(
-        local_patient_id
         local_patient_id_2
         local_patient_id_3
         local_patient_id_4
