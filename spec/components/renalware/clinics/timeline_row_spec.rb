f5ff91789671faf902a7319234adb5818ba8712f
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-09-25 13:42:21 +0100

    Add ClinicVisit notes to timeline
    Closes #5469

--- spec/components/renalware/clinics/timeline_row_spec.rb
+++ spec/components/renalware/clinics/timeline_row_spec.rb
@@ -5,14 +5,24 @@ module Renalware
     subject { described_class.new(sort_date:, record:) }
 
     let(:sort_date) { Date.new(2025, 7, 9) }
-    let(:record) { build(:clinic_visit) }
+    let(:location) { build(:clinic_visit_location, :teams) }
+    let(:record) do
+      build(
+        :clinic_visit,
+        location: location,
+        notes: "Good progress with treatment",
+        admin_notes: "Patient arrived on time"
+      )
+    end
     let(:created_by) { record.created_by.full_name }
     let(:clinic) { record.clinic.name }
 
     it "renders component" do
-      expect(table_fragment.css(".toggler i")).to be_empty
+      expect(table_fragment.css(".toggler i")).not_to be_empty
       expect(table_fragment.text).to include("09-Jul-2025Clinic Visit#{clinic}#{created_by}")
-      expect(table_fragment.css(".hidden").text).to be_empty
+      expect(table_fragment.css(".hidden").text).to include("Location:Over Teams")
+      expect(table_fragment.css(".hidden").text).to include("Notes:Good progress with treatment")
+      expect(table_fragment.css(".hidden").text).to include("Admin notes:Patient arrived on time")
     end
 
     context "when dietetic clinic visit" do
