d757e161c77f323a56d99c92d6ef67b21f8fc44a
Tim Crowe <tim@woodpigeon.com>
2025-10-01 13:12:15 +0100

    Add paper trail support for RolesUser and update specs accordingly
    If a role is added to or removed from a user, capture this in system_versions using PaperTrail. If the changes if via the Admin UI then introduce a workaround to make sure we get the destroy callbacks that will generate an associated PaperTrail entry

--- spec/models/renalware/roles_user_spec.rb
+++ spec/models/renalware/roles_user_spec.rb
@@ -4,6 +4,7 @@ module Renalware
       is_expected.to belong_to :user
       is_expected.to belong_to :role
       is_expected.to respond_to(:id)
+      is_expected.to be_versioned
     end
   end
 end
