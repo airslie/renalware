d797cb0c336042799b95008845c5334861dfcc84
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-10-16 10:40:04 +0100

    Add long comment on my Devise woes

--- app/models/concerns/renalware/deviseable.rb
+++ app/models/concerns/renalware/deviseable.rb
@@ -19,6 +19,23 @@ module Renalware
           timeoutable
         )
 
+        # Having conditional modules makes testing really hard. This is because
+        # once the models are loaded you can't switch them on/off. This was
+        # highlighted when attempting to disable LDAP authentication for devops
+        # users. This meant reintroducing database_authenticatable and that
+        # meant it conflicted with the LDAP module.
+        # From the LDAP Authenticatable docs:
+        # | This devise plugin has not been tested with DatabaseAuthenticatable
+        # | enabled at the same time
+        # Specifically, calling `super` in `valid_password?` causes the LDAP
+        # `valid_password?` method to be called instead of the database one.
+        #
+        # My recommendation is to have all modules loaded unconditionally and
+        # then turn off what's needed with flags. E.g. recoverable doesn't show
+        # if DEVISE_USER_RECOVERABLE=false and relevant controllers are disabled.
+        #
+        # LDAP Authenticatable is 5 years old so probably better to use the bits
+        # that are needed it works differently to how we're using it.
         if Renalware.config.ldap_authentication
           modules << :ldap_authenticatable
         else
