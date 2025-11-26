02ed8a07c01e2453817d105f8246ac1500607d9c
Tim Crowe <tim@woodpigeon.com>
2025-11-26 12:04:53 +0000

    Require omniauth

--- demo/config/initializers/devise.rb
+++ demo/config/initializers/devise.rb
@@ -3,8 +3,8 @@ Devise.setup do |config|
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
   config.secret_key = "abcdff0441bcc45be7f0b05f40c6107fbed63df50eb7023a2aa09237fad4f108b2cc2516e9aa"
-  config.omniauth_path_prefix = "/users/auth"
 
+  config.omniauth_path_prefix = "/users/auth"
   config.omniauth(
     :entra_id,
     client_id: ENV.fetch("ENTRA_CLIENT_ID", nil),
