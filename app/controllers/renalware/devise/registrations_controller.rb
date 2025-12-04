f0421ebbeffda0e6f8ea90fedb41e32e40e34587
Tim Crowe <tim@woodpigeon.com>
2025-12-04 12:58:44 +0000

    Raise 404 for devise sign_up routes if LDAP enabled

--- app/controllers/renalware/devise/registrations_controller.rb
+++ app/controllers/renalware/devise/registrations_controller.rb
@@ -1,4 +1,20 @@
 class Renalware::Devise::RegistrationsController < Devise::RegistrationsController
   helper Renalware::ApplicationHelper
   include Renalware::Concerns::DeviseControllerMethods
+
+  def new
+    if Renalware.config.ldap_authentication
+      raise ActionController::RoutingError, "User registration is not available."
+    else
+      super
+    end
+  end
+
+  def create
+    if Renalware.config.ldap_authentication
+      raise ActionController::RoutingError, "User registration is not available."
+    else
+      super
+    end
+  end
 end
