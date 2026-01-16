c3b5b4245a503aa3a0e3f399db3a37d8c3f6a9d5
Tim Crowe <tim@woodpigeon.com>
2026-01-16 16:41:46 +0000

    Remove explicit Devise.secret_key
    As this defaults to Rails.application.secret_key_base which will look at SECRET_KEY_BASE which is we want, as we do not want to use Rails secrets or credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,12 +1,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  # The secret key used by Devise. Devise uses this key to generate
-  # random tokens. Changing this key will render invalid all existing
-  # confirmation, reset password and unlock tokens in the database.
-  # Set this in the host application and add to a values stored in secrets
-  # config.secret_key = 'xxx'
-
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
