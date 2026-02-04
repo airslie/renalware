1488d9215f67aa883f86850aba27ae1a469dd5cc
Tim Crowe <tim@woodpigeon.com>
2026-02-04 18:50:25 +0000

    Housekeeping

--- config/initializers/slim.rb
+++
@@ -1,6 +0,0 @@
-# See https://github.com/slim-template/slim#shortcuts
-# Slim::Engine.set_options shortcut: {
-#   "@" => { attr: "role" },
-#   "#" => { attr: "id" },
-#   "." => { attr: "class" }
-# }
