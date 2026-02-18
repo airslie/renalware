e41fe6f7f7603c636cd87044388712e08905b1e0
Tim Crowe <tim@woodpigeon.com>
2026-02-18 13:17:11 +0000

    Fix cucumber spec

--- features/support/test_depth_guard.rb
+++
@@ -1,9 +0,0 @@
-Before("@web or @legacy") do
-  next if ENV["TEST_DEPTH"] == "web"
-
-  raise <<~MSG
-    Web cucumber scenarios require TEST_DEPTH=web.
-    Run:
-      TEST_DEPTH=web bundle exec cucumber -p rake_web features
-  MSG
-end
