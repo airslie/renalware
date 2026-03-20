54a7bad8bc44ee1c58fabf3017532c560e51823f
Tim Crowe <tim@woodpigeon.com>
2026-03-20 10:22:06 +0000

    Fail to boot in prod if certain ENV vars are missing
    Now that we are totally ENV-driven, it becomes important to test that,
    if we really are running in a production environment,
    key configuration values (like the map that determines which
    local_patient_id* maps to which assigning authority) are present,
    otherwise we should refuse to boot because there is the possibility for
    example that we might fail to ingest incoming if that mapping is
    missing.
    
    Note we only validate ENV vars if RENALWARE_STAGE is set to production.
    We don't use RAILS_ENV=production because that could be in true
    in UAT and demo environments, or anytime the docker image is started.

---
+++ lib/renalware/required_env.rb
@@ -0,0 +1,38 @@
+module Renalware
+  # Since Renalware is totally ENV-driven, it becomes important to test that,
+  # if we really are running in a production environment,
+  # key configuration values (like the map that determines which
+  # local_patient_id* maps to which assigning authority) are present,
+  # otherwise we should refuse to boot because there is the possibility for
+  # example that we might fail to ingest incoming if that mapping is
+  # missing.
+  # Note we only validate ENV vars if RENALWARE_STAGE is set to production.
+  # We don't use RAILS_ENV=production because that could be in true
+  # in UAT and demo environments, or anytime the docker image is started.
+  module RequiredEnv
+    PRODUCTION_STAGE = "production".freeze
+    REQUIRED_IN_PRODUCTION = %w(
+      PATIENT_HOSPITAL_IDENTIFIERS
+      SECRET_KEY_BASE
+      HL7_PID_SEX_MAP
+    ).freeze
+
+    module_function
+
+    def validate!
+      return unless production_stage?
+
+      missing_keys = REQUIRED_IN_PRODUCTION.select { |key| ENV[key].blank? }
+      return if missing_keys.empty?
+
+      raise(
+        KeyError,
+        "Missing required ENV vars for RENALWARE_STAGE=production: #{missing_keys.join(', ')}"
+      )
+    end
+
+    def production_stage?
+      ENV["RENALWARE_STAGE"].to_s.casecmp?(PRODUCTION_STAGE)
+    end
+  end
+end
