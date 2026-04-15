13915201369e54fe5a1d4a16ea6b39829ca27552
Tim Crowe <tim@woodpigeon.com>
2026-04-15 12:05:17 +0100

    Add donor DOB to transplant_offer event

--- spec/models/renalware/transplants/offer_spec.rb
+++ spec/models/renalware/transplants/offer_spec.rb
@@ -5,5 +5,22 @@ describe Renalware::Transplants::Offer do
     subject { described_class.new.document }
 
     it { is_expected.to validate_presence_of(:donor_id) }
+
+    it "allows donor_dob to be blank" do
+      document = described_class.new.document
+      document.donor_id = "NHSBT-12345"
+      document.donor_dob = nil
+
+      expect(document).to be_valid
+    end
+
+    it "does not allow donor_dob to be in the future" do
+      document = described_class.new.document
+      document.donor_id = "NHSBT-12345"
+      document.donor_dob = 1.day.from_now.to_date
+
+      expect(document).not_to be_valid
+      expect(document.errors[:donor_dob]).to include("must be on or before #{Date.current}")
+    end
   end
 end
