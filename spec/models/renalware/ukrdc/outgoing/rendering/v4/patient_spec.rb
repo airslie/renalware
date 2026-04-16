f6006faf54316a0d3fb25f9891bef1c5dc025ff4
Tim Crowe <tim@woodpigeon.com>
2026-04-16 18:25:56 +0100

    Add VascularAccess to UKRDC XML

--- spec/models/renalware/ukrdc/outgoing/rendering/v4/patient_spec.rb
+++ spec/models/renalware/ukrdc/outgoing/rendering/v4/patient_spec.rb
@@ -4,6 +4,10 @@ module Renalware
       describe Rendering::V4::Patient do
         include XmlSpecHelper
 
+        def render_xml(patient)
+          format_xml(described_class.new(patient:).xml)
+        end
+
         it "assigns the patient a renal_registry_id if the don't have one" do
           patient = Renalware::UKRDC::PatientPresenter.new(
             create(:patient, renal_registry_id: nil)
@@ -23,81 +27,55 @@ module Renalware
           described_class.new(patient:).xml
           expect(patient.reload.renal_registry_id).to eq("123")
         end
+
+        it "includes the correctly formatted NHS number" do
+          patient = Renalware::UKRDC::PatientPresenter.new(
+            create(:patient, nhs_number: "6433678181", sent_to_ukrdc_at: 1.year.ago)
+          )
+
+          xml = render_xml(patient)
+
+          expect(xml).to include("<PatientNumbers>")
+          expect(xml).to include("<Number>6433678181</Number>")
+          expect(xml).to include("<Organization>NHS</Organization>")
+          expect(xml).to include("<NumberType>NI</NumberType>")
+        end
+
+        it "outputs PrimaryLanguage" do
+          language = create(:language, :english)
+          patient = Renalware::UKRDC::PatientPresenter.new(
+            create(
+              :patient,
+              sent_to_ukrdc_at: 1.year.ago,
+              language: language
+            )
+          )
+
+          xml = render_xml(patient)
+
+          expect(xml).to include("<PrimaryLanguage>")
+          expect(xml)
+            .to include("<CodingStandard>NHS_DATA_DICTIONARY_LANGUAGE_CODE</CodingStandard>")
+          expect(xml).to include("<Code>en</Code>")
+        end
+
+        context "when the language is Other (ot)" do
+          it "does not output it" do
+            language = create(:language, :other)
+            patient = Renalware::UKRDC::PatientPresenter.new(
+              create(
+                :patient,
+                sent_to_ukrdc_at: 1.year.ago,
+                language: language
+              )
+            )
+
+            xml = render_xml(patient)
+
+            expect(xml).not_to include("<PrimaryLanguage>")
+          end
+        end
       end
     end
   end
 end
-
-# TODO: include these tests from the old builder view spec
-# it "includes the correctly formatted NHS number" do
-#   patient = Renalware::UKRDC::PatientPresenter.new(
-#     build(
-#       :patient,
-#       nhs_number: "9999999999",
-#       sent_to_ukrdc_at: 1.year.ago
-#     )
-#   )
-#   render partial: "renalware/api/ukrdc/patients/patient.xml.builder",
-#          locals: {
-#            patient: patient,
-#            builder: Builder::XmlMarkup.new
-#          }
-
-#   expect(rendered).to include("<Number>9999999999</Number>")
-# end
-
-# it "includes omits NHS number if patient has none" do
-#   patient = Renalware::UKRDC::PatientPresenter.new(
-#     build(
-#       :patient,
-#       nhs_number: nil,
-#       sent_to_ukrdc_at: 1.year.ago,
-#       local_patient_id: nil
-#     )
-#   )
-#   render partial: "renalware/api/ukrdc/patients/patient.xml.builder",
-#          locals: {
-#            patient: patient,
-#            builder: Builder::XmlMarkup.new
-#          }
-
-#   expect(rendered).not_to include("<Number>")
-# end
-
-# it "outputs PrimaryLanguage" do
-#   language = build_stubbed(:language, :english)
-#   patient = Renalware::UKRDC::PatientPresenter.new(
-#     build(
-#       :patient,
-#       sent_to_ukrdc_at: 1.year.ago,
-#       language: language
-#     )
-#   )
-#   render partial: "renalware/api/ukrdc/patients/patient.xml.builder",
-#          locals: {
-#            patient: patient,
-#            builder: Builder::XmlMarkup.new
-#          }
-
-#   expect(rendered).to include("<PrimaryLanguage>")
-# end
-
-# context "when the language is Other (ot)" do
-#   it "does not output it" do
-#     language = build_stubbed(:language, :other)
-#     patient = Renalware::UKRDC::PatientPresenter.new(
-#       build(
-#         :patient,
-#         sent_to_ukrdc_at: 1.year.ago,
-#         language: language
-#       )
-#     )
-#     render partial: "renalware/api/ukrdc/patients/patient.xml.builder",
-#            locals: {
-#              patient: patient,
-#              builder: Builder::XmlMarkup.new
-#            }
-
-#     expect(rendered).not_to include("<PrimaryLanguage>")
-#   end
-# end
